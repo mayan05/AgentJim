fa42e5819e24b2ea46068553a0879cca32371777
Mayan Sequeira <mayan.sequeira@gmail.com>
2025-08-13 22:27:30 +0530

    added nutri agent

--- src/config/settings.py
+++ src/config/settings.py
@@ -1,17 +1,29 @@
 from pydantic import BaseModel
 from langchain_openai import OpenAI
 from langchain_anthropic import AnthropicLLM
+from langchain_google_genai import ChatGoogleGenerativeAI
 import os
 
 class Settings(BaseModel):
     openai_api_key: str | None = os.environ.get("OPENAI_API_KEY", None)
     anthropic_api_key: str | None = os.environ.get("ANTHROPIC_API_KEY", None)
+    gemini_api_key: str | None = os.environ.get("GEMINI_API_KEY", None)
 
     openai_model: str = "gpt-3.5-turbo-instruct"
     anthropic_model: str = "claude-2"
+    gemini_model: str = "gemini-2.5-pro"
 
     temp: float = 0.7
 
-    openai_llm: OpenAI = OpenAI(temperature=temp, model_name=openai_model, max_tokens=2000, api_key=openai_api_key)
+    openai_llm: OpenAI = OpenAI(temperature=temp, 
+    model_name=openai_model, max_tokens=2000, 
+    api_key=openai_api_key)
 
-    anthropic_llm: AnthropicLLM = AnthropicLLM(temperature=temp, model_name=anthropic_model, api_key=anthropic_api_key, max_tokens=2000)
+    anthropic_llm: AnthropicLLM = AnthropicLLM(temperature=temp, model_name=anthropic_model, 
+    api_key=anthropic_api_key, 
+    max_tokens=2000)
+
+    gemini_llm: ChatGoogleGenerativeAI = ChatGoogleGenerativeAI(google_api_key=gemini_api_key,
+    temperature=temp,
+    max_tokens=2000, 
+    max_retries=2)
