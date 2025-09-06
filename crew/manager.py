571efd6d327ee047e057e41a471787705bebc3c3
Mayan Sequeira <mayan.sequeira@gmail.com>
2025-09-06 10:59:20 +0530

    gotta update crews manager

--- crew/manager.py
+++ crew/manager.py
@@ -15,8 +15,11 @@ crew = Crew(
     memory=False  # Disabled due to path length issue
 )
 
-if __name__ == "__main__":
-    crew.kickoff({"input": """Hi! I'm Sarah, a 28-year-old female. I'm 5'6" tall and currently weigh 150 lbs. I'd like to get down to around 140 lbs and also build some lean muscle tone. 
+
+
+    
+# TEST PROMPT
+"""Hi! I'm Sarah, a 28-year-old female. I'm 5'6" tall and currently weigh 150 lbs. I'd like to get down to around 140 lbs and also build some lean muscle tone. 
 
 I'm pretty much a beginner when it comes to structured workouts - I used to go for occasional jogs and did some yoga classes pre-pandemic, but haven't been consistent for the past 2 years. Right now I'd say I'm lightly active at best.
 
@@ -28,4 +31,5 @@ As for diet, I'm not vegetarian but I don't eat red meat often. I have no major 
 
 I usually skip breakfast, have a light lunch, and tend to eat my biggest meal at dinner. I'm open to changing this if it helps with my goals. Budget-wise, I'm fine with reasonable nutrition costs but nothing too expensive.
 
-Can you help me create a workout and nutrition plan?"""})
+Can you help me create a workout and nutrition plan?"""
+# shall use this for testing later on
